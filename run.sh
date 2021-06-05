151b5275cb859c765fff7ce484a57d9ede36afd2
deep-learning-server <jm84.joo@gmail.com>
2021-06-06 05:15:50 +0900

    update port info

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm -d -p 6379:6379 --hostname=redis-store --name=redis-store aibakevision/redis-store:0.0.1
+docker run --rm -d -p 5004:6379 --hostname=redis-store --name=redis-store aibakevision/redis-store:0.0.1
